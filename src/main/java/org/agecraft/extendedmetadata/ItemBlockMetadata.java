3b7ac0225f2388cde6de9de3b981ec3baf9ef16b
DanieHuisman <danielhuisman.development@gmail.com>
2015-07-04 10:33:30 +0200

    Added basic extended metadata block and item

---
+++ src/main/java/org/agecraft/extendedmetadata/ItemBlockMetadata.java
@@ -0,0 +1,37 @@
+package org.agecraft.extendedmetadata;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+public class ItemBlockMetadata extends ItemBlock {
+
+	private BlockMetadata block;
+
+    public ItemBlockMetadata(Block block) {
+        super(block);
+        this.block = (BlockMetadata) block;
+        setMaxDamage(0);
+        setHasSubtypes(true);
+    }
+
+    @Override
+    public BlockMetadata getBlock() {
+        return block;
+    }
+
+    @Override
+    public int getMetadata(int meta) {
+        return meta;
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack stack) {
+        return block.getLocalizedName(stack.getItemDamage());
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return block.getUnlocalizedName(stack.getItemDamage());
+    }
+}
